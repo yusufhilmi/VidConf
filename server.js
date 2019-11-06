1a4702ab7e1e02baf84a1bff830cc9f8f31f66b0
Yusuf Hilmi <177yusuf177@gmail.com>
2019-11-06 17:23:55 +0300

    credentials

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var OpenViduRole = require('openvidu-node-client').OpenViduRole;
 var TokenOptions = require('openvidu-node-client').TokenOptions;
 
 // Check launch arguments: must receive openvidu-server URL and the secret
-if (process.argv.length != 4) {
-    console.log("Usage: node " + __filename + " OPENVIDU_URL OPENVIDU_SECRET");
-    process.exit(-1);
-}
+//  if (process.argv.length != 4) {
+//     console.log("Usage: node " + __filename + " OPENVIDU_URL OPENVIDU_SECRET");
+//     process.exit(-1);
+//  }
 // For demo purposes we ignore self-signed certificate
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
@@ -41,7 +41,7 @@ var options = {
     key: fs.readFileSync('openvidukey.pem'),
     cert: fs.readFileSync('openviducert.pem')
 };
-https.createServer(options, app).listen(9001);
+https.createServer(options, app).listen(5000);
 
 // Mock database
 var users = [{
@@ -59,9 +59,9 @@ var users = [{
 }];
 
 // Environment variable: URL where our OpenVidu server is listening
-var OPENVIDU_URL = process.argv[2];
+var OPENVIDU_URL = 'https://demos.openvidu.io:4443';
 // Environment variable: secret shared with our OpenVidu server
-var OPENVIDU_SECRET = process.argv[3];
+var OPENVIDU_SECRET = 'MY_SECRET';
 
 // Entrypoint to OpenVidu Node Client SDK
 var OV = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
@@ -71,7 +71,7 @@ var mapSessions = {};
 // Collection to pair session names with tokens
 var mapSessionNamesTokens = {};
 
-console.log("App listening on port 9001");
+console.log("App listening on port 5000");
 
 /* CONFIGURATION */
 
